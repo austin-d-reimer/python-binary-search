9950c1480d82666a7c5bffe06f40e8d2c0ba50bb
Austin Reimer <austin.d.reimer@gmail.com>
2021-01-06 14:39:21 -0600

    Added a user search method to search based on userinputed number

--- run_program.py
+++ run_program.py
@@ -1,12 +1,34 @@
 from binary_search import search
 from random_sorted_array import create_random_sorted_array
 
-arr = [2, 3, 4, 10, 40, 45]
-target = 46
 
-search(arr, target)
+def user_search():
+    search(arr2, int(input("Number to search for ")))
 
-arr = create_random_sorted_array(1_000_000, 50_000)
-target = arr[1200]
+    user_input = input("Do you want to continue Y or n ")
 
-search(arr, target)
+    if user_input == "n":
+        print("Good bye")
+        return
+    else:
+        user_search()
+
+
+arr1 = [2, 3, 4, 10, 40, 45]
+target1 = 46
+
+search(arr1, target1)
+
+arr2 = create_random_sorted_array(1_000_000, 100_000)
+target2 = arr2[1200]
+
+print(
+    f"""
+length {len(arr2)}
+max {arr2[-1]}
+"""
+)
+
+search(arr2, target2)
+
+user_search()
